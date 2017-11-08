2b61039c927b960505754f6297016a892904b683
Jorge <Gallardo.Jorge4@gmail.com>
2017-11-07 22:06:30 -0600

    Movimiento rectangulo con flechas

--- Personal/main.lua
+++ Personal/main.lua
@@ -1,10 +1,22 @@
 function love.load()
-    
+    X=200
+    Y=100
     end
-function love.update()
-    
+function love.update(dt)
+    if love.keyboard.isDown("right") then 
+    X=X+2    
+    end
+    if love.keyboard.isDown("left") then 
+    X=X-2    
+    end
+    if love.keyboard.isDown("up") then 
+    Y=Y-2    
+    end
+    if love.keyboard.isDown("down") then 
+    Y=Y+2    
+    end
     end
 function love.draw()
-    love.graphics.print("Hola Mundo",love.mouse.getX(),love.mouse.getY()    )
+    love.graphics.rectangle("fill", X,Y,50,70)
     
     end
