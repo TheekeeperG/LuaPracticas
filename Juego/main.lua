ea29274300ce5577c1a46d5cc39d404fa4deba71
Jorge <Gallardo.Jorge4@gmail.com>
2017-11-08 00:16:24 -0600

    Metoros con timing

--- Juego/main.lua
+++ Juego/main.lua
@@ -2,15 +2,29 @@ function love.load()
     
     require "personaje"
     require "meteoro"
+    math.randomseed(os.time())
     personaje:init(100,100)
-    meteoro1 =meteoro:new(10,10)
+    mts={}
+    tiempodegeneracion=20
     end
 
 function love.update(dt)
-    personaje:update(dt)
+   if tiempodegeneracion==20 then 
+    local x=math.random(1,400)
+    local y=math.random(1,300)
+    tiempodegeneracion=20
+    table.insert(mts, meteoro:new(x,y))
+  
+else 
+    tiempodegeneracion=tiempodegeneracion-1
+    end
+   
     end 
 
 function love.draw()
     personaje:draw()
-    meteoro1:draw()
+    for i,v in ipairs(mts) do
+    v:draw()
+    end
+    
     end
