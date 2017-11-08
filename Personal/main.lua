8565f62a7d4e90d0bafb77568cfd4e151ec16c1a
Jorge <Gallardo.Jorge4@gmail.com>
2017-11-07 22:36:26 -0600

    Movimiento con flachas y colision

--- Personal/main.lua
+++ Personal/main.lua
@@ -1,22 +1,49 @@
 function love.load()
-    X=200
-    Y=100
+    x=100 
+    y=100 
+    w=30
+    h=30
+    
+    bx=300 
+    by=100
+    bw=50
+    bh=100
+    
+    cx=true
+    cy=true
+    cxm=true
+    cym=true
+    
+    msg=":D"
     end
 function love.update(dt)
-    if love.keyboard.isDown("right") then 
-    X=X+2    
-    end
-    if love.keyboard.isDown("left") then 
-    X=X-2    
-    end
-    if love.keyboard.isDown("up") then 
-    Y=Y-2    
-    end
-    if love.keyboard.isDown("down") then 
-    Y=Y+2    
+    
+    
+   if love.keyboard.isDown("right") then 
+    x=x+100*dt
+    end 
+    if love.keyboard.isDown("left")  then 
+    x=x-100*dt
+    end 
+    if love.keyboard.isDown("up")  then 
+    y=y-100*dt
+    end 
+    if love.keyboard.isDown("down")then 
+    y=y+100*dt
+    end 
+    if x<bx+bw and
+        x+w>bx and
+        y<by+bh and 
+        y + h>by then 
+        msg = "Auch :c"
+        
+    else 
+        msg="Mejor :D"
     end
     end
 function love.draw()
-    love.graphics.rectangle("fill", X,Y,50,70)
-    
+   love.graphics.rectangle("fill",x,y,w,h)
+    love.graphics.rectangle("fill",bx,by,bw,bh)
+        
+    love.graphics.print(msg)
     end
